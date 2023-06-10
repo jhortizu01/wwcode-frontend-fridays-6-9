3c7cbcdb5f1c4de9a0d0bb4472e181bbf4b0f426
Janika Hortizuela <jhortizuela21@gmail.com>
2023-06-10 13:52:26 -0600

    Complete file set up, menu

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Menu } from './Menu';
+
+function App() {
+  return (
+    <div className='app'>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Menu.jsx
@@ -0,0 +1,8 @@
+export const Menu = () => {
+  return (
+    <div className='menu'>
+      <span class='material-symbols-outlined'>menu</span>
+      <h1>Steps</h1>
+    </div>
+  );
+};
